9e73bd37cc36df65d51e3580195c1a713008b6f8
Nitin Londhe <nitin.londhe@genmills.com>
2022-02-09 00:11:02 +0530

    added svelete page with css

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mainContainer">
+    <div class="dataContainer">
+      <div class="header">
+        👋 Hey, I'm Nitin!
+      </div>
+      <div class="bio">
+        I am .Net developer learning blockchain.
+      </div>
+      <div class="bio">         
+        Connect your Ethereum wallet and wave at me!
+      </div>
+      <div class="dataContainer">
+          <button class="waveButton">Connect Wallet</button>
+      </div>      
+      <div class="bio">Deployed on Rinkeby at 0x0000.</div>
+    </div>
+</div>
+
+<style>    
+    .mainContainer {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        margin-top: 64px;
+    }
+
+    .dataContainer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        max-width: 600px;
+        margin-top: 32px;
+        margin-bottom: 10px;
+    }
+
+    .header {
+        text-align: center;
+        font-size: 32px;
+        font-weight: 600;
+    }
+
+    .bio {
+        text-align: center;
+        color: gray;
+        margin-top: 16px;
+    }
+
+    .waveButton {
+        cursor: pointer;
+        margin-top: 16px;
+        padding: 16px;
+        border: 0;
+        border-radius: 10px;
+        font-size: 15px;
+        color: purple;
+    }
+</style>
