2800d72b8f1b9325f93cfaf988356098c5a8aad5
Nitin Londhe <nitin.londhe@genmills.com>
2022-02-09 01:04:48 +0530

    css changes

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -18,40 +18,49 @@
 
 <style>    
     .mainContainer {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        margin-top: 64px;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-top: 64px;
     }
 
     .dataContainer {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        max-width: 600px;
-        margin-top: 32px;
-        margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 600px;
+    margin-top: 32px;
+    margin-bottom: 10px;
+
     }
 
     .header {
-        text-align: center;
-        font-size: 32px;
-        font-weight: 600;
+    text-align: center;
+    font-size: 32px;
+    font-weight: 600;
     }
 
     .bio {
-        text-align: center;
-        color: gray;
-        margin-top: 16px;
+    text-align: center;
+    color: gray;
+    margin-top: 16px;
+    }
+
+    .waveMessage {
+    margin-top: 16px;
+    padding: 8px;
+    border: 0;
+    border-radius: 5px;
+    
     }
 
     .waveButton {
-        cursor: pointer;
-        margin-top: 16px;
-        padding: 16px;
-        border: 0;
-        border-radius: 10px;
-        font-size: 15px;
-        color: purple;
+    cursor: pointer;
+    margin-top: 16px;
+    padding: 16px;
+    border: 0;
+    border-radius: 10px;
+    font-size: 15px;
+    color: purple;
     }
 </style>
