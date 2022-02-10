b6b80f268115ebe5cbf24fc36ae5a3f7e0cab995
Nitin Londhe <nitin.londhe@genmills.com>
2022-02-11 02:24:23 +0530

    added logic to connect account via metamask

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -1,3 +1,56 @@
+<script>
+    import { onMount } from 'svelte';
+
+    var currentAccount;
+
+    onMount(async () => {
+		await checkIfWalletIsConnected();
+	});
+
+    async function checkIfWalletIsConnected() {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                console.log("Make sure you have metamask!");
+                return;
+            } else {
+                console.log("We have the ethereum object", ethereum);
+            }
+
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log("Found an authorized account:", account);
+                currentAccount = account;
+            } else {
+                console.log("No authorized account found")
+            }
+        } catch (error) {
+            console.log(error);
+        }
+  }
+    
+    async function connectWallet() {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                alert("Get MetaMask!");
+                return;
+            }
+
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+            console.log("Connected", accounts[0]);
+            currentAccount = accounts[0];
+        } catch (error) {
+            console.log(error)
+        }
+    }
+</script>
+
 <div class="mainContainer">
     <div class="dataContainer">
       <div class="header">
@@ -9,10 +62,16 @@
       <div class="bio">         
         Connect your Ethereum wallet and wave at me!
       </div>
-      <div class="dataContainer">
-          <button class="waveButton">Connect Wallet</button>
-      </div>      
-      <div class="bio">Deployed on Rinkeby at 0x0000.</div>
+      {#if currentAccount == null}
+        <div class="dataContainer">
+            <button class="waveButton" on:click={connectWallet}>Connect Wallet</button>
+        </div>   
+      {:else}
+        <div class="dataContainer">
+            <button class="waveButton">Wave at Me</button>
+        </div>   
+      {/if}         
+      <div class="bio">Deployed on Rinkeby at 0x06852D209C524c37EB7D328ED3d1571c773d9F99</div>
     </div>
 </div>
 
