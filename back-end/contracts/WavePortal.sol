7d8ce47e8173fd03a2ebb8986fcc865b25ca459b
Nitin Londhe <nitin.londhe@genmills.com>
2022-02-18 02:35:06 +0530

    added seed and cooldown

--- back-end/contracts/WavePortal.sol
+++ back-end/contracts/WavePortal.sol
@@ -13,26 +13,55 @@ contract WavePortal {
         uint256 timestamp; // The timestamp when the user waved.
     }
 
+    uint256 private seed;
+    mapping(address => uint256) private lastWavedAt;
+
     Wave[] waves;
     uint public totalWaves;
 
     constructor() payable {
         console.log("We have been constructed!");
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s waved w/ message %s", msg.sender, _message);
 
-        uint256 prizeAmount = 0.0001 ether;
-
-        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
-
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-
-        require(success, "Failed to withdraw money from contract.");
-
         waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+
+            require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
+
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+
+            require(success, "Failed to withdraw money from contract.");
+        }
 
         emit NewWave(msg.sender, _message, block.timestamp);
     }
--- back-end/scripts/run.js
+++ back-end/scripts/run.js
@@ -24,13 +24,16 @@ const main = async () => {
   /*
    * Send Wave
    */
-  let waveTxn = await waveContract.wave("A message!");
+  let waveTxn = await waveContract.wave("A message 1 !");
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.wave("A message 2 !");
   await waveTxn.wait();
 
   /*
    * Send Another Wave
    */
-  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message !");
   await waveTxn.wait();
 
   /*
