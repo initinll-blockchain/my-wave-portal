c07e42fa57c799f92d54044154efda4354ee5b1c
Nitin Londhe <nitin.londhe@genmills.com>
2022-02-25 21:03:20 +0530

    Initial Commit with Typescript support

--- front-end/src/lib/components/Footer.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import { getContractAddress } from '$lib/helper/wavePortalHelper';
-    let contractAddress = getContractAddress();
-</script>
-
-<div class="bio">Deployed on Rinkeby at {contractAddress}</div>
--- front-end/src/lib/components/Header.svelte
+++
@@ -1,9 +0,0 @@
-<div class="header">
-    👋 Hey, I'm Nitin! 
-</div>
-<div class="bio">
-    I am .Net developer learning blockchain.
-</div>
-<div class="bio">
-    Connect your Ethereum wallet and wave at me!
-</div>
